9f829fa1500bba85ac12443e5e23b0d91db19e98
Juan Jose Reyes Corredor <juanj.reyes@softtekprojects.com>
2022-04-06 16:41:54 -0500

    Adding Driver Factory Class to ExampleTest

--- src/test/java/tests/ExampleTest.java
+++ src/test/java/tests/ExampleTest.java
@@ -2,16 +2,19 @@ package tests;
 
 import org.testng.annotations.Test;
 
+import framework.core.DriverManagerFactory;
+import framework.core.drivers.DriverManager;
+import framework.enums.DriverType;
 import pages.RegisterPage;
 
 import org.testng.annotations.BeforeTest;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 
 public class ExampleTest {
-	
+	//Driver manager class
+	DriverManager driverManager;
 	WebDriver driver;
 	RegisterPage register;
   @Test
@@ -23,8 +26,9 @@ public class ExampleTest {
   }
   @BeforeTest
   public void beforeTest() {
-	 System.setProperty("webdriver.chrome.driver","C:\\Users\\guillermow.gonzalez\\Downloads\\chromedriver.exe");
-	 driver = new ChromeDriver();
+	  //Init ChromeDriver from Factory pattern
+	 driverManager = DriverManagerFactory.getDriverManager(DriverType.CHROME);
+	 driver = driverManager.getWebDriver();
 	 register = new RegisterPage(driver);
   }
 
